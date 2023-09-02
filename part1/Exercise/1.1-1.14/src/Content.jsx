dbf381ad8f1c241ce9de4d3e9d8ebd1c444d8d45
Carl Saginsin <saguinsincarl8@gmail.com>
2023-09-02 08:39:56 +0700

    Exercises 1.1 to 1.14 answers.

---
+++ part1/Exercise/1.1-1.14/src/Content.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Part({ part, exercises }) {
+    return (
+      <p>
+        {part} {exercises}
+      </p>
+    );
+  }
+  
+  function Content() {
+    return (
+      <div>
+        <Part part="Fundamentals of React" exercises={10} />
+        <Part part="Using props to pass data" exercises={7} />
+        <Part part="State of a component" exercises={14} />
+      </div>
+    );
+  }
+
+export default Content
---
+++ part1/Exercise/1.1-1.14/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    const course = 'Give Feedback'
+  return (
+    <>
+        <h1>{course}</h1>
+    </>
+  )
+}
+
+export default Header
